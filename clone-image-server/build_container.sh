f73e275584f8a28a9332a3740dbc76a59b0f2af4
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-06-26 14:12:15 -0400

    Dockerizing clone image server

---
+++ clone-image-server/build_container.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker build -t kalki/kalki-clone-image-server .
---
+++ clone-image-server/run_container.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker run  -p 5000:5000 --rm --name kalki-clone-image-server kalki/kalki-clone-image-server
