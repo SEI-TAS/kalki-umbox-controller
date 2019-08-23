31b975fbdd6df403fa00f5f806928e26e2f3cd64
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-08-23 15:50:40 -0400

    Added dhcp server restart

--- run_container.sh
+++ run_container.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
+
+# DHCP server sometimes does not start properly along with machine.
+sudo service isc-dhcp-server restart
+
 docker run -p 6060:6060 --rm --net=host --name kalki-uc kalki/kalki-uc
