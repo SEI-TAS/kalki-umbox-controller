e07b9839098fba535e7c4fbe0f553bc826e2b33a
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-08-23 16:05:51 -0400

    Moving DHCP restart to proper server

--- clone-image-server/run.sh
+++ clone-image-server/run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# DHCP server sometimes does not start properly along with machine.
+sudo service isc-dhcp-server restart
+
 export PIPENV_VENV_IN_PROJECT="enabled"
 python -m pipenv install
 python -m pipenv run python clone_image_api.py
--- run_container.sh
+++ run_container.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env bash
-
-# DHCP server sometimes does not start properly along with machine.
-sudo service isc-dhcp-server restart
-
 docker run -p 6060:6060 --rm --net=host --name kalki-uc kalki/kalki-uc
