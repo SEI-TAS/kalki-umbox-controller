5c7a46018b8e32da81c16bcf2e402dd0bd08ff65
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2019-10-03 14:35:33 -0400

    Added cleanup to clone image server

--- clone-image-server/run.sh
+++ clone-image-server/run.sh
@@ -3,6 +3,19 @@
 # DHCP server sometimes does not start properly along with machine.
 sudo service isc-dhcp-server restart
 
+# Setup pipenv env in case this is the first time this is called.
 export PIPENV_VENV_IN_PROJECT="enabled"
 python -m pipenv install
+
+# Clean up any leftover VMs.
+echo "Removing all running umboxes with images still in this folder."
+INSTANCE_IMAGES="./images/instances/*.qcow2"
+for FILEPATH in ${INSTANCE_IMAGES}; do
+    BASENAME=$(basename $FILEPATH .qcow2)
+    echo "Removing VM with name ${BASENAME}"
+    sudo virsh destroy $BASENAME
+    echo "Removing image file ${FILEPATH}"
+    rm -f $FILEPATH
+done
+
 python -m pipenv run python clone_image_api.py
