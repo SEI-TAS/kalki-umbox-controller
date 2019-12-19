af872daca2703cff14a7d0561d19e3219f8ca6af
Sebastian Echeverria <secheverria@sei.cmu.edu>
2019-12-19 17:14:36 -0500

    First version of DockerUmbox that can properly start and stop a container. Locally for this version.

--- src/main/java/edu/cmu/sei/kalki/uc/umbox/DockerUmbox.java
+++ src/main/java/edu/cmu/sei/kalki/uc/umbox/DockerUmbox.java
@@ -3,20 +3,26 @@ package edu.cmu.sei.kalki.uc.umbox;
 import com.amihaiemil.docker.Container;
 import com.amihaiemil.docker.Containers;
 import com.amihaiemil.docker.Docker;
+import com.amihaiemil.docker.LocalDocker;
 import com.amihaiemil.docker.RemoteDocker;
 import edu.cmu.sei.kalki.uc.utils.Config;
 import edu.cmu.sei.ttg.kalki.models.Device;
 import edu.cmu.sei.ttg.kalki.models.UmboxImage;
 
+import javax.json.JsonObject;
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Iterator;
 
 public class DockerUmbox extends Umbox
 {
     private static final String PREFIX = "umbox-";
 
     private static Docker docker;
+    private String umboxContainerName;
+
     public DockerUmbox(UmboxImage image, Device device)
     {
         super(image, device);
@@ -31,11 +37,14 @@ public class DockerUmbox extends Umbox
 
     private void connectToDocker()
     {
+        umboxContainerName = "/" + PREFIX + String.valueOf(this.umboxId);
+
         try
         {
-            docker = new RemoteDocker(new URI(Config.data.get("data_node_ip")));
+            //docker = new RemoteDocker(new URI(Config.data.get("data_node_ip")));
+            docker = new LocalDocker(new File("/var/run/docker.sock"));
         }
-        catch(URISyntaxException e)
+        catch(Exception e) //URISyntaxException
         {
             System.out.println("Error processing data node IP: " + e.getMessage());
             e.printStackTrace();
@@ -50,7 +59,7 @@ public class DockerUmbox extends Umbox
             docker.containers().create(PREFIX + this.umboxId, image.getName());
             return true;
         }
-        catch (IOException e)
+        catch (Exception e)
         {
             System.out.println("Error creating docker container: " + e.getMessage());
             e.printStackTrace();
@@ -62,17 +71,41 @@ public class DockerUmbox extends Umbox
     protected boolean stop()
     {
         Containers containers  = docker.containers();
-        for(Container container : containers)
+        Iterator<Container> allContainers = containers.all();
+        while(allContainers.hasNext())
         {
-            if(container.getString("name").equals(String.valueOf(this.umboxId)))
+            Container container = allContainers.next();
+            JsonObject containerInfo;
+            try
+            {
+                containerInfo = container.inspect();
+            } catch (IOException e)
+            {
+                System.out.println("Error getting container information: " + e.toString());
+                e.printStackTrace();
+                return false;
+            }
+
+            if(umboxContainerName.equals(containerInfo.getString("Name")))
             {
                 try
                 {
                     container.stop();
-                    return true;
-                } catch (IOException e)
+                }
+                catch (Exception e)
                 {
-                    System.out.println("Problem stopping container.");
+                    System.out.println("Could not stop container; assuming already stopped.");
+                }
+
+                try
+                {
+                    container.remove();
+                    System.out.println("Container successfully removed.");
+                    return true;
+                }
+                catch (IOException e)
+                {
+                    System.out.println("Problem removing container.");
                     e.printStackTrace();
                     return false;
                 }
@@ -81,5 +114,31 @@ public class DockerUmbox extends Umbox
 
         System.out.println("Container was not found.");
         return false;
+    }
+
+    public static void test()
+    {
+        try
+        {
+            Device dev = new Device();
+            UmboxImage image = new UmboxImage();
+            image.setName("hello-world");
+
+            System.out.println("Starting container");
+            DockerUmbox u = new DockerUmbox(image, dev);
+            u.start();
+
+            System.out.println("Waiting for a bit.");
+            Thread.sleep(5000);
+
+            System.out.println("Stopping and removing container.");
+            u.stop();
+
+            System.out.println("Finished");
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
