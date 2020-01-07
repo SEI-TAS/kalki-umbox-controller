cc11f3d7af30cd7160c2dece1d70b9c892462929
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-01-07 14:45:40 -0500

    Created test case for docker, hid unused tool

---
+++ tests/test-docker.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE FUNCTION setCurrentState()
+    RETURNS VOID AS $$
+DECLARE
+    normal RECORD;
+    suspicious RECORD;
+    attack RECORD;
+    dtype RECORD;
+    d RECORD;
+    dSS RECORD;
+    uimage RECORD;
+BEGIN
+    SELECT INTO normal id FROM security_state WHERE name='Normal';
+    SELECT INTO suspicious id FROM security_state WHERE name='Suspicious';
+    SELECT INTO attack id FROM security_state WHERE name='Attack';
+    SELECT INTO dtype id FROM device_type WHERE name='Udoo Neo';
+    INSERT INTO device(name, description, type_id, ip_address, status_history_size, sampling_rate, default_sampling_rate) VALUES ('UNTS1', 'Test Udoo', dtype.id, '10.27.151.101', 1, 10000, 10000) RETURNING id INTO d;
+    INSERT INTO device_security_state (device_id, state_id, timestamp) VALUES (d.id, normal.id, current_timestamp) RETURNING id INTO dSS;
+    UPDATE device set current_state_id=dSS.id WHERE id=d.id;
+    INSERT INTO umbox_image(name, file_name) VALUES ('hello-world', '') RETURNING id INTO uimage;
+    INSERT INTO umbox_lookup(state_id, device_type_id, umbox_image_id, dag_order) VALUES (normal.id, dtype.id, uimage.id, 1);
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT setCurrentState();
