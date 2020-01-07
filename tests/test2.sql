8f17917a02595c1a2a99f5f67f54cd050a87fe45
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-01-07 15:32:13 -0500

    Consistency with removed column

--- tests/test2.sql
+++ tests/test2.sql
@@ -17,7 +17,6 @@ BEGIN
     SELECT INTO dtype id FROM device_type WHERE name='DLink Camera';
     INSERT INTO device(name, description, type_id, ip_address, status_history_size, sampling_rate, default_sampling_rate) VALUES ('CAM1', 'Test cam', dtype.id, '10.27.151.114', 1, 10000, 10000) RETURNING id INTO d;
     INSERT INTO device_security_state (device_id, state_id, timestamp) VALUES (d.id, normal.id, current_timestamp) RETURNING id INTO dSS;
-    UPDATE device set current_state_id=dSS.id WHERE id=d.id;
     INSERT INTO umbox_image(name, file_name) VALUES ('u14 DLC Max Login Detection', 'u14-dlc-max-login.qcow2') RETURNING id INTO u6;
     INSERT INTO umbox_image(name, file_name) VALUES ('u3-http-auth-proxy-block', 'u3-http-auth-proxy-block.qcow2') RETURNING id INTO u7;
     INSERT INTO umbox_image(name, file_name) VALUES ('u4-block-all', 'u4-block-all.qcow2') RETURNING id INTO u4;
