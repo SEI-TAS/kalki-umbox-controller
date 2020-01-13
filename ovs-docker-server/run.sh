d2b0f372c6423705b8fab8d211abb6d220483502
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-01-13 11:35:35 -0500

    Remove running container umboxes when starting ovs server

--- ovs-docker-server/run.sh
+++ ovs-docker-server/run.sh
@@ -5,6 +5,6 @@ sudo service isc-dhcp-server restart
 
 # Clean up any leftover containers.
 echo "Removing all running umboxes."
-# TODO
+docker ps --format '{{.Names}}' | grep "^umbox-" | awk '{print $1}' | xargs -I {} docker stop {}
 
 python -m pipenv run python docker_api.py
