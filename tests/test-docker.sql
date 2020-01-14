796a9177c1ea07da1542b072452661ce2d034af6
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-01-14 13:38:03 -0500

    Changed test umbox name

--- tests/test-docker.sql
+++ tests/test-docker.sql
@@ -15,7 +15,7 @@ BEGIN
     SELECT INTO dtype id FROM device_type WHERE name='Udoo Neo';
     INSERT INTO device(name, description, type_id, ip_address, status_history_size, sampling_rate, default_sampling_rate) VALUES ('UNTS1', 'Test Udoo', dtype.id, '10.27.151.101', 1, 10000, 10000) RETURNING id INTO d;
     INSERT INTO device_security_state (device_id, state_id, timestamp) VALUES (d.id, normal.id, current_timestamp) RETURNING id INTO dSS;
-    INSERT INTO umbox_image(name, file_name) VALUES ('u5', '') RETURNING id INTO uimage;
+    INSERT INTO umbox_image(name, file_name) VALUES ('u5-sniffer-log-stats', '') RETURNING id INTO uimage;
     INSERT INTO umbox_lookup(state_id, device_type_id, umbox_image_id, dag_order) VALUES (normal.id, dtype.id, uimage.id, 1);
 END;
 $$ LANGUAGE plpgsql;
