c9814d4a2a93989696421c905cd2322cf9c16712
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-01-27 16:01:28 -0500

    Added support for building container supporting either VM or Docker umboxes

--- container_run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-HOST_TZ=$(cat /etc/timezone)
-docker run -p 6060:6060 --rm --net=host -e TZ=${HOST_TZ} -v ~/kalki/kalki-umbox-controller/tests:/app/umbox_controller-1.0-SNAPSHOT/tests --name kalki-uc kalki/kalki-uc $1 $2
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+HOST_TZ=$(cat /etc/timezone)
+docker run -p 6060:6060 --rm --net=host -e TZ=${HOST_TZ} -v ~/kalki/kalki-umbox-controller/tests:/app/umbox_controller-1.0-SNAPSHOT/tests --name kalki-uc $1 $2 $3
