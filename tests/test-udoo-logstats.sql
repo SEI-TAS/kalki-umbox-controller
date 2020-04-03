eb68a709e1a7b42688d34aebefd0ed3e8943d871
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-04-03 16:19:31 -0400

    SQL error in cases

--- tests/test-udoo-logstats.sql
+++ tests/test-udoo-logstats.sql
@@ -15,7 +15,7 @@ BEGIN
     SELECT INTO attack id FROM security_state WHERE name='Attack';
     SELECT INTO dtype id FROM device_type WHERE name='Udoo Neo';
     INSERT INTO data_node(name, ip_address) VALUES('Hertz', '10.27.153.2') RETURNING id INTO dnodeid;
-    INSERT INTO device(name, description, type_id, ip_address, status_history_size, sampling_rate, default_sampling_rate, data_node_id) VALUES ('UNTS1', 'Test Udoo', dtype.id, '10.27.151.101', 1, 10000, 10000, dnodeid) RETURNING id INTO d;
+    INSERT INTO device(name, description, type_id, ip_address, status_history_size, sampling_rate, default_sampling_rate, data_node_id) VALUES ('UNTS1', 'Test Udoo', dtype.id, '10.27.151.101', 1, 10000, 10000, dnodeid.id) RETURNING id INTO d;
     INSERT INTO device_security_state (device_id, state_id, timestamp) VALUES (d.id, normal.id, current_timestamp) RETURNING id INTO dSS;
     INSERT INTO umbox_image(name, file_name) VALUES ('u5-sniffer-log-stats', '') RETURNING id INTO uimage;
     INSERT INTO umbox_lookup(security_state_id, device_type_id, umbox_image_id, dag_order) VALUES (normal.id, dtype.id, uimage.id, 1);
