86ce25785f96e95841a728d3aac5188b7db3bf55
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-04-07 17:06:35 -0400

    Adding dockerization to ovs-docker-server

---
+++ ovs-docker-server/build_container.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker build -t kalki/kalki-ovs-docker-server .
--- ovs-docker-server/run.sh
+++ ovs-docker-server/run.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# DHCP server sometimes does not start properly along with machine.
-sudo service isc-dhcp-server restart
-
 # Clean up any leftover containers.
 echo "Removing all running umboxes."
 docker ps --format '{{.Names}}' | grep "^umbox-" | awk '{print $1}' | xargs -I {} docker stop {}
