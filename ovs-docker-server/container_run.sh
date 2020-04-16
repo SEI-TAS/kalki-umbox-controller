5123c537e1ee51c5746e9cef676a11fc76d34767
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-04-15 22:39:55 -0400

    Mounting network namespace folder

--- ovs-docker-server/container_run.sh
+++ ovs-docker-server/container_run.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 docker container stop kalki-ovs-docker-server
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /var/run/openvswitch/db.sock:/var/run/openvswitch/db.sock --cap-add=NET_ADMIN --network=host --name kalki-ovs-docker-server kalki/kalki-ovs-docker-server
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock \
+                -v /var/run/openvswitch/db.sock:/var/run/openvswitch/db.sock \
+                -v /var/run/netns/:/var/run/netns/ \
+                --cap-add=NET_ADMIN --network=host \
+                --name kalki-ovs-docker-server kalki/kalki-ovs-docker-server
