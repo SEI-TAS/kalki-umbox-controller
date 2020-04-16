3bbee710b1eea40bb0ff7d7f3bf7af31df60ffc4
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-04-15 22:30:37 -0400

    Mapping ovs socket

--- ovs-docker-server/container_run.sh
+++ ovs-docker-server/container_run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker container stop kalki-ovs-docker-server
-docker run -p 5500:5500 --rm -v /var/run/docker.sock:/var/run/docker.sock --cap-add=NET_ADMIN --network=host --name kalki-ovs-docker-server kalki/kalki-ovs-docker-server
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /var/run/openvswitch/db.sock:/var/run/openvswitch/db.sock --cap-add=NET_ADMIN --network=host --name kalki-ovs-docker-server kalki/kalki-ovs-docker-server
