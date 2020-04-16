46beeb2dd24e8f4705ef598d5b44051bd6385b6e
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-04-15 22:02:45 -0400

    Adding ovs-docker to image, as well as linking it to the host network

--- ovs-docker-server/container_run.sh
+++ ovs-docker-server/container_run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -p 5500:5500 --rm -v /var/run/docker.sock:/var/run/docker.sock --name kalki-ovs-docker-server kalki/kalki-ovs-docker-server
+docker run -p 5500:5500 --rm -v /var/run/docker.sock:/var/run/docker.sock --cap-add=NET_ADMIN --network=host --name kalki-ovs-docker-server kalki/kalki-ovs-docker-server
