dc548928f49581b57ff9afd48052fe688213b270
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-04-16 12:04:55 -0400

    Adding all permissions

--- ovs-docker-server/run_container.sh
+++ ovs-docker-server/run_container.sh
@@ -4,7 +4,6 @@ bash ./ovs-scripts/setup_bridge.sh
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock \
                 -v /var/run/openvswitch/db.sock:/var/run/openvswitch/db.sock \
                 -v /proc:/host_proc \
-                --cap-add=NET_ADMIN --network=host \
-                --cap-add=DAC_OVERRIDE \
-                --cap-add=DAC_READ_SEARCH \
+                --network=host \
+                --privileged \
                 --name kalki-ovs-docker-server kalki/kalki-ovs-docker-server
