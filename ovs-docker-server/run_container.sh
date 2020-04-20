5c95fed9781487e73db124230b79067ccf9d763d
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-04-20 16:20:56 -0400

    Consistency with call

--- ovs-docker-server/run_container.sh
+++ ovs-docker-server/run_container.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 bash stop.sh
-bash ./ovs-scripts/setup_bridge.sh
+cd ./ovs-scripts
+bash setup_bridge.sh
+cd ..
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock \
                 -v /var/run/openvswitch/db.sock:/var/run/openvswitch/db.sock \
                 -v /proc:/host_proc \
