2914c987a28266a6ce29222c98a9adab8d0f2a4f
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-04-20 15:47:07 -0400

    Moved network config to separate file, and some configs to config.sh

--- ovs-docker-server/setup.sh
+++ ovs-docker-server/setup.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 
-source config.sh
-
-DOCKER_CONTROL_NET=control-net
-CONTROL_NET=${CONTROL_NET_PREFIX}.0/24
-GATEWAY=${CONTROL_NET_PREFIX}.1
-IP_RANGE=${CONTROL_NET_PREFIX}.128/25
-
-docker network create -d macvlan --subnet=${CONTROL_NET} \
-                                 --gateway=${GATEWAY} \
-                                 --ip-range=${IP_RANGE} \
-                                 -o parent=${CONTROL_NIC} \
-                                 ${DOCKER_CONTROL_NET}
+bash setup_docker_network.sh
 
 ENV PIPENV_VENV_IN_PROJECT "enabled"
 pipenv install
