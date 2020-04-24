1ff8281f6f434d90aa535230c3490beb6cca66de
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-04-24 10:43:40 -0400

    Naming and docker parms consistency, updating path

--- ovs-docker-server/run_container.sh
+++ ovs-docker-server/run_container.sh
@@ -8,9 +8,11 @@ bash setup_bridge.sh
 cd ..
 
 # Start container.
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock \
-                -v /var/run/openvswitch/db.sock:/var/run/openvswitch/db.sock \
-                -v /proc:/host_proc \
+HOST_TZ=$(cat /etc/timezone)
+docker run --rm -e TZ=${HOST_TZ} \
                 --network=host \
                 --privileged \
+                -v /var/run/docker.sock:/var/run/docker.sock \
+                -v /var/run/openvswitch/db.sock:/var/run/openvswitch/db.sock \
+                -v /proc:/host_proc \
                 --name kalki-ovs-docker-server kalki/kalki-ovs-docker-server
--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 HOST_TZ=$(cat /etc/timezone)
-docker run -p 6060:6060 --rm --net=host -e TZ=${HOST_TZ} -v ~/kalki/kalki-umbox-controller/tests:/app/kalki-umbox-controller-1.0-SNAPSHOT/tests --name kalki-uc $1 $2 $3
+docker run --rm -e TZ=${HOST_TZ} \
+           --network=host \
+           -v ~/kalki/kalki-umbox-controller/tests:/app/kalki-umbox-controller-1.4/tests \
+           --name kalki-uc $1 $2 $3
