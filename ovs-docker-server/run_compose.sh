049941f5e56a47d998092fce75930c19d567e6cc
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-05-08 16:18:59 -0400

    Config has to be loaded earlier

--- ovs-docker-server/run_compose.sh
+++ ovs-docker-server/run_compose.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 source prepare_env.sh
+export HOST_TZ=$(cat /etc/timezone)
 docker-compose up -d
 bash compose_logs.sh
--- ovs-docker-server/teardown_env.sh
+++ ovs-docker-server/teardown_env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Stop container, clear umboxes, clear bridge.
-bash clear_umboxes.sh
 source config.sh
+bash clear_umboxes.sh
 (cd ./ovs-scripts && bash remove_bridge.sh)
