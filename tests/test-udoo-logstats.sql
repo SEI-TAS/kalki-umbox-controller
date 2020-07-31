ca1aad44ad997dd25b89206501937c3f75e201cf
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-07-31 16:00:37 -0400

    Updating tests for local ip

--- tests/test-udoo-logstats.sql
+++ tests/test-udoo-logstats.sql
@@ -14,7 +14,7 @@ BEGIN
     SELECT INTO suspicious id FROM security_state WHERE name='Suspicious';
     SELECT INTO attack id FROM security_state WHERE name='Attack';
     SELECT INTO dtype id FROM device_type WHERE name='Udoo Neo';
-    INSERT INTO data_node(name, ip_address) VALUES('Local', '127.0.0.1') RETURNING id INTO dnodeid;
+    INSERT INTO data_node(name, ip_address) VALUES('Local', '10.27.153.2') RETURNING id INTO dnodeid;
     INSERT INTO device(name, description, type_id, ip_address, status_history_size, sampling_rate, default_sampling_rate, data_node_id) VALUES ('UNTS1', 'Test Udoo', dtype.id, '10.27.151.101', 1, 10000, 10000, dnodeid.id) RETURNING id INTO d;
     INSERT INTO device_security_state (device_id, state_id, timestamp) VALUES (d.id, normal.id, current_timestamp) RETURNING id INTO dSS;
     INSERT INTO umbox_image(name, file_name) VALUES ('u5-sniffer-log-stats', '') RETURNING id INTO uimage;
