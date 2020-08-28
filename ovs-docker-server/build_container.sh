2bc85738826eb05582c2071324a19fb769ea6304
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-08-28 16:02:45 -0400

    Adding skip tests flag

--- ovs-docker-server/build_container.sh
+++ ovs-docker-server/build_container.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-docker build -t kalki/kalki-ovs-docker-server .
+
+SKIP_TESTS_ARG=""
+if [ "$1" == "--skip_tests" ]; then
+  SKIP_TESTS_ARG=" -x test "
+fi
+
+docker build --build-arg SKIP_TESTS="${SKIP_TESTS_ARG}" -t kalki/kalki-ovs-docker-server .
 docker pull busybox:latest
