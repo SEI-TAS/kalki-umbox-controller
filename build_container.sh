1458bf1bbd3e741e50f1b47c21f93e3ca6726b5c
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-09-15 13:24:14 -0400

    Missing quotes

--- build_container.sh
+++ build_container.sh
@@ -4,4 +4,4 @@ if [ "$1" == "--skip_tests" ]; then
   SKIP_TESTS_ARG=" -x test "
 fi
 
-docker build --network=host --build-arg SKIP_TESTS=${SKIP_TESTS_ARG} -t kalki/kalki-umbox-controller .
+docker build --network=host --build-arg SKIP_TESTS="${SKIP_TESTS_ARG}" -t kalki/kalki-umbox-controller .
