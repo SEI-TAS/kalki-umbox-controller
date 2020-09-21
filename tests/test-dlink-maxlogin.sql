f90dcf8d0a6ab237305199d075c88f4ede440135
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-09-21 10:04:17 -0400

    Updating to include credentials where needed

--- tests/test-dlink-maxlogin.sql
+++ tests/test-dlink-maxlogin.sql
@@ -17,7 +17,7 @@ BEGIN
     SELECT INTO attack id FROM security_state WHERE name='Attack';
     SELECT INTO dtype id FROM device_type WHERE name='DLink Camera';
     INSERT INTO data_node(name, ip_address) VALUES('Hertz', '10.27.153.2') RETURNING id INTO dnodeid;
-    INSERT INTO device(name, description, type_id, ip_address, status_history_size, sampling_rate, default_sampling_rate, data_node_id) VALUES ('CAM1', 'Test cam', dtype.id, '10.27.151.114', 1, 10000, 10000, dnodeid.id) RETURNING id INTO d;
+    INSERT INTO device(name, description, type_id, ip_address, status_history_size, sampling_rate, default_sampling_rate, data_node_id, credentials) VALUES ('CAM1', 'Test cam', dtype.id, '10.27.151.114', 1, 10000, 10000, dnodeid.id, '') RETURNING id INTO d;
     INSERT INTO device_security_state (device_id, state_id, timestamp) VALUES (d.id, normal.id, current_timestamp) RETURNING id INTO dSS;
     INSERT INTO umbox_image(name, file_name) VALUES ('u14-dlc-max-login', 'u14-dlc-max-login.qcow2') RETURNING id INTO u1;
     INSERT INTO umbox_image(name, file_name) VALUES ('u3-http-auth-proxy-block', 'u3-http-auth-proxy-block.qcow2') RETURNING id INTO u2;
