3089480667b2f91b726f4e13058537da288f9d5c
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-11-24 14:03:53 -0500

    Updating kalki-db version

--- build_container.sh
+++ build_container.sh
@@ -4,7 +4,7 @@ if [ "$1" == "--skip_tests" ]; then
   SKIP_TESTS_ARG=" -x test "
 fi
 
-KALKI_DB_VER="1.7.0"
+KALKI_DB_VER="1.8.0"
 docker build --network=host \
              --build-arg SKIP_TESTS="${SKIP_TESTS_ARG}" \
              --build-arg KALKI_DB_VER="${KALKI_DB_VER}" \
